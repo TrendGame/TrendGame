1e1d2d09832f74de5de87a7661afcc4691885c77
Eric Pan <epan@users.noreply.github.com>
2017-04-26 11:43:00 -0700

    (feat) server setup (#4)
    * (feat) Use express to set up server
    
    * (feat) Add HTTP req logging and body parsing
    
    * (feat) Serve static files in public folder

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+const IP = '127.0.0.1';
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/client/public'));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${IP}:${PORT}`);
+});
