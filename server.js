e35f08788c39d7ee2fae07f8689c57472c79fd68
Eric Pan <epan@users.noreply.github.com>
2017-04-27 12:02:09 -0700

    (refactor) specify test paths (#21)

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ app.use(express.static(__dirname + '/client/public'));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${IP}:${PORT}`);
-});
+if (!module.parent) {
+  app.listen(PORT, () => {
+    console.log(`Listening on ${IP}:${PORT}`);
+  });
+}
 
 app.get('/api', (req, res) => {
   res.send({
