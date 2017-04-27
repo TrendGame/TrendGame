aa3b6c376b0bf7ac015c9f54a3f49951bb23f71f
BhattPrateek <prateekbhatt94@gmail.com>
2017-04-26 19:44:31 -0700

    (refactor) Remove div tag types from class names.  (#16)
    * (refactor)Attempt to synchronize with remote master.
    
    * (refactor)Refactor needed class names to kebab case and remove extraneous ones.
    
    * (refactor)Remove tag-types from class names.

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -3,7 +3,27 @@ import {render} from 'react-dom';
 
 class App extends React.Component {
   render () {
-    return <p>HELLO ___ REACT!</p>;
+    return (
+      <div>
+        <div>
+          <h1>TrendGame!</h1>
+        </div>
+        <div className="search-bar">
+          <form>
+            <input type="text" name="Explore Trends" placeholder="Explore Your Interests"/>
+          </form>
+        </div>
+        <div className="main">
+          <div className="graph">
+            <h2>GRAPH___GOES___HERE!</h2>
+          </div>
+          <div className="article">
+            <h2>ARTICLES___GO___HERE!</h2>
+          </div>
+        </div>
+
+      </div>
+    );
   }
 }
 
