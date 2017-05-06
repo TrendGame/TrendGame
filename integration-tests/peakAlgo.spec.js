2efc67e313ff7e48bdd2def134f43fad68f5e3c7
Eric Pan <epan@users.noreply.github.com>
2017-05-06 11:03:03 -0700

    (refactor) Remove sample data trend query (#33)
    What: Deleted "sample-data" folder and the file representing trend
    query sanitized results. Updated Peak Algo test to not reference it
    and no other files reference deleted directory.
    
    Why: Already have a fixtures folder with sanitized trend query
    results inside the testing folder which seems more logical. Now
    we clear up a top-level directory from the project.

--- integration-tests/peakAlgo.spec.js
+++ integration-tests/peakAlgo.spec.js
@@ -1,14 +1,14 @@
 var chai = require('chai');
 
 var app = require('../server.js');
-var findPeak = require('../utilities/peakAlgo.js');
-var dummyData = require('../sample-data/trend-query-output.js');
+var findPeak = require('../utilities/peakAlgo');
+var dummyData = require('./fixtures/trend-sanitized');
 
 var expect = chai.expect;
 
 describe('Peak Algo', () => {
   it('should return peak value and associated date as a tuple in an array', () => {
-    var expected = [[1478390400, 100]];
+    var expected = [[1464480000, 100]];
     var actual = findPeak(dummyData);
 
     expect(actual).to.eql(expected);
