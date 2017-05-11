df8be54fb99d44e0a8a30093fdbf5155e1c70c43
Alex Seiden <alex.seiden@gmail.com>
2017-05-11 14:36:05 -0700

    (feat) render message if no stories found for trend

--- client/app/components/ArticleList.jsx
+++ client/app/components/ArticleList.jsx
@@ -4,17 +4,22 @@ import Article from './Article.jsx';
 const ArticleList = ({ storyPoint }) => {
   let articles;
 
-  !storyPoint.hasOwnProperty('stories')
-  ? articles = <div></div>
-  : articles = (
+  if (storyPoint.hasOwnProperty('stories') && storyPoint.stories[0] === null) {
+    articles = <div>
+      <h2>No stories found for this trend.</h2>
+    </div>;
+  } else if (storyPoint.hasOwnProperty('stories')) {
+    articles = (
       <div>
         <h2>Top news stories for {storyPoint.formattedTime}</h2>
         {storyPoint.stories.map(story => {
           return <Article key={story.url} story={story}/>;
         })}
       </div>
-  );
-
+    );
+  } else {
+    articles = <div></div>;
+  }
   return articles;
 };
 
