cd3bba537c94ad85162dd93d9fed1d564d6562b8
Alex Seiden <alex.seiden@gmail.com>
2017-05-10 19:00:42 -0700

    Refactor/db heroku config (#50)
    * (feat) add pg to package.json
    
    * (feat) ignore local db config file
    
    * (feat) stub db config dummy
    
    * (feat) connect to postgres db locally and in deployed env
    
    * (bug) address bugs in db connection code
    
    * (feat) create db schema via command line script

--- db/setupDatabase.js
+++ db/setupDatabase.js
@@ -1,23 +1,50 @@
 //set up the database if not exist
-var serverUrl = '127.0.0.1';
-var connection = {
-  host: serverUrl,
-  user: 'root',
-  password: '',
-  charset: 'utf8'
-};
-var knex = require('knex')({
-  client: 'mysql',
-  connection: connection,
-  useNullAsDefault: true
+// This file should be run once from the command line to initialize the db schema.  Be sure to edit the url const with the target url.
+const pg = require('pg');
+
+const url = '<INSERT DB URL HERE FOR SCHEMA UPLOAD>';
+
+var db = require('knex')({
+  client: 'pg',
+  connection: url + '?ssl=true'
 });
-knex.raw('CREATE DATABASE IF NOT EXISTS trendNewsDB')
-  .then(function () {
-    connection.database = 'trendNewsDB';
-    knex = require('knex')({ client: 'mysql', connection: connection });
-  }).catch(function (err) {
-    console.log(err);
-  });
-connection.database = 'trendNewsDB';
-knex = require('knex')({ client: 'mysql', connection: connection });
-module.exports = knex;
+
+db.schema.hasTable('trends').then(function (exists) {
+  if (!exists) {
+    db.schema.createTable('trends', function (trend) {
+      trend.increments('id').primary();
+      trend.string('name');
+      trend.timestamps();
+    }).then(function (table) {
+      console.log('Created Table trends');
+    });
+  }
+});
+db.schema.hasTable('weeks').then(function (exists) {
+  if (!exists) {
+    db.schema.createTable('weeks', function (week) {
+      week.increments('id').primary();
+      week.string('startDate');
+      week.integer('popularity');
+      week.integer('trendId').unsigned();
+      week.foreign('trendId').references('trends.id');
+    }).then(function (table) {
+      console.log('created Table weeks');
+    });
+  }
+});
+db.schema.hasTable('stories').then(function (exists) {
+  if (!exists) {
+    db.schema.createTable('stories', function (story) {
+      story.increments('id').primary();
+      story.string('articleName', 500);
+      story.string('mediaUrl', 200);
+      story.string('url', 500);
+      story.string('previewText', 1000);
+      story.integer('weeksId').unsigned();
+      story.foreign('weeksId').references('weeks.id');
+    }).then(function (table) {
+      console.log('created Table stories');
+    });
+  }
+});
