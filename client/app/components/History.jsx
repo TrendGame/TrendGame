c86acd0cf5872f7c5f48fe10aec4b79d3d04fef9
Eric Pan <epan@users.noreply.github.com>
2017-05-10 18:50:22 -0700

    (feat) Add history bar placeholder component (#51)

---
+++ client/app/components/History.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const History = (props) => {
+  return (
+    <div>
+      <h2>History goes here</h2>
+      <ul>
+        <li>corgis</li>
+        <li>puppers</li>
+      </ul>
+    </div>
+  );
+};
+
+export default History;
--- client/app/components/Layout.jsx
+++ client/app/components/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Body from './Body';
 import Footer from './Footer';
 import Header from './Header';
+import History from './History';
 
 export default class Layout extends React.Component {
   render () {
     return (
       <div>
         <Header/>
+        <History/>
         <Body
           chartData={this.props.chartData}
           collectData={this.props.collectData}
