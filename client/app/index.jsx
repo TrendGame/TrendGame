cf1800a2d16eb2e889d05e9ab531a4250d51dd4b
Eric Pan <epan@users.noreply.github.com>
2017-05-10 19:07:29 -0700

    (refactor) rename test folder and scripts (#54)

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import axios from 'axios';
-import data from '../../integration-tests/fixtures/stitched-timeline';
 import Layout from './components/Layout';
 
 class App extends React.Component {
