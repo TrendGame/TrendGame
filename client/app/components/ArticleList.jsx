ce9526f0121a4d5462eb0ed5b4ad3dfb34c47ab7
Alex Seiden <alex.seiden@gmail.com>
2017-05-11 16:00:21 -0700

    Feat/null stories handler (#62)
    * (feat) render message if no stories found for trend
    
    * (feat) render date
    
    * (refactor) add parenths
    
    * (bug) remove bad semi

--- client/app/components/ArticleList.jsx
+++ client/app/components/ArticleList.jsx
@@ -4,19 +4,24 @@ import Article from './Article.jsx';
 const ArticleList = ({ storyPoint }) => {
   let articles;
 
-  !storyPoint.hasOwnProperty('stories')
-  ? articles = <div></div>
-  : articles = (
-      <div className="row">
-        <div className="col-12">
-          <h3>Top stories from {storyPoint.formattedAxisTime}</h3>
-          {storyPoint.stories.map(story => {
-            return <Article key={story.url} story={story}/>;
-          })}
-        </div>
+  if (storyPoint.hasOwnProperty('stories') && storyPoint.stories[0] === null) {
+    articles = (
+      <div>
+        <h2>No stories on this trend found for {storyPoint.formattedTime}.</h2>
       </div>
-  );
-
+    );
+  } else if (storyPoint.hasOwnProperty('stories')) {
+    articles = (
+      <div>
+        <h2>Top news stories for {storyPoint.formattedTime}</h2>
+        {storyPoint.stories.map(story => {
+          return <Article key={story.url} story={story}/>;
+        })}
+      </div>
+    );
+  } else {
+    articles = <div></div>;
+  }
   return articles;
 };
 
