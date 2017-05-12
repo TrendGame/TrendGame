8578c6b9a1d91e3f5179b1c7ed5117c3da7c71ff
Eric Pan <epan@users.noreply.github.com>
2017-05-12 15:44:58 -0700

    (feat) Make recent searches into links

--- client/app/components/HistoryItem.jsx
+++ client/app/components/HistoryItem.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const HistoryItem = ({ term, index }) => {
   let element = index === 0
-    ? <li className="list-inline-item">{term}</li>
-    : <li className="list-inline-item">&middot; {term}</li>;
+    ? (
+        <li className="list-inline-item">
+          <a href="#" className="text-muted">{term}</a>
+        </li>
+      )
+    : (
+        <li className="list-inline-item">
+          &middot; <a href="#" className="text-muted">{term}</a>
+        </li>
+      );
 
   return element;
 };
