ddcbb6a49623a51619f23d0d1bc9cad9abf4b7ae
Eric Pan <epan@users.noreply.github.com>
2017-05-11 18:18:21 -0700

    Feat/articles layout (#67)
    * (bug) Fix merge conflict mistake I made
    
    * (feat) Show 2 articles per row with col-6
    
    * (refactor) Make proper rows of articles
    
    * (feat) Make article columns count responsive
    
    * (refactor) Remove lodash dependency
    
    * (bug) Remove import to lodash
    
    * (refactor) Remove comment unused function

--- client/app/components/Article.jsx
+++ client/app/components/Article.jsx
@@ -4,12 +4,18 @@ const Article = ({ story }) => {
   let { headline, url, media, summary } = story;
 
   return (
-    <div className="card" style={{width: '20rem'}}>
-      <img className="card-img-top img-fluid" src={media} alt={headline}/>
-      <div className="card-block">
-        <h4 className="card-title">{headline}</h4>
-        <p className="card-text">{summary}</p>
-        <a href={url} className="btn btn-primary">Read more</a>
+    <div className="col-12 col-lg-6">
+      <div className="card">
+        <img
+          className="card-img-top img-fluid"
+          src={media}
+          alt={headline}
+        />
+        <div className="card-block">
+          <h4 className="card-title">{headline}</h4>
+          <p className="card-text">{summary}</p>
+          <a href={url} className="card-link">Read more</a>
+        </div>
       </div>
     </div>
   );
