192c209e876a0ec7d34b7977285ae27bd4cee8bc
Eric Pan <epan@users.noreply.github.com>
2017-05-12 14:55:01 -0700

    (refactor) Clean up ternary

--- client/app/components/HistoryItem.jsx
+++ client/app/components/HistoryItem.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const HistoryItem = ({ term, index }) => {
-  let element = '';
-
-  index === 0
-  ? element = <li className="list-inline-item">{term}</li>
-  : element = <li className="list-inline-item">&middot; {term}</li>;
+  let element = index === 0
+    ? <li className="list-inline-item">{term}</li>
+    : <li className="list-inline-item">&middot; {term}</li>;
 
   return element;
 };
