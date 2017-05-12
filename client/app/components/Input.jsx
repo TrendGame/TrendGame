2f00dc19e4f10a1a77807cba401647e47340ae3d
bhattprateek <prateekbhatt94@gmail.com>
2017-05-11 18:28:17 -0700

    (feat)Submit user input on return press.

--- client/app/components/Input.jsx
+++ client/app/components/Input.jsx
@@ -8,6 +8,7 @@ export default class Input extends React.Component {
     };
     this.handeInput = this.handeInput.bind(this);
     this.submitTrend = this.submitTrend.bind(this);
+    this.onEnter = this.onEnter.bind(this);
   }
 
   handeInput(event) {
@@ -18,12 +19,19 @@ export default class Input extends React.Component {
     this.props.collectData(this.state.trend);
   }
 
+  onEnter(event) {
+    console.log(event.key);
+    if( event.key === 'Enter' ){
+      this.props.collectData(this.state.trend);
+    }
+  }
+
   render () {
     return (
       <div className="row">
         <div className="col-12">
           <h6>Search for a trend</h6>
-          <input placeholder="Enter a topic" onChange={this.handeInput}></input>
+          <input placeholder="Enter a topic" onKeyPress={this.onEnter} onChange={this.handeInput}></input>
           <input type="submit" onClick={this.submitTrend}></input>
         </div>
       </div>
