cbc5d5cf8b7c977f8e77e63b23b320b638771755
Alex Seiden <alex.seiden@gmail.com>
2017-05-12 18:19:11 -0700

    Bug/handle null trends (#85)
    * (bug) return null when no trend found
    
    * (bug) remove error throw in stitchData

--- utilities/stitchData.js
+++ utilities/stitchData.js
@@ -1,12 +1,11 @@
 module.exports = (timeline, stories) => {
   const timelineDates = timeline.map(point => point.date);
 
-  stories.forEach(story => {
-    // throw error if a story's date is not in the timeline
-    if (!timelineDates.includes(story.date)) {
-      throw new Error(`Story date matches no timeline dates: ${story}`);
-    }
+  if (timeline.length === 0) {
+    return null;
+  }
 
+  stories.forEach(story => {
     // add stories to matching the timeline date
     timeline.forEach(point => {
       if (point.date === story.date) {
